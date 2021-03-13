5a570f5df12c3eebfa0023c28ac1ce0da934f8fb
Richard Ng <richardkentng@gmail.com>
2021-03-13 12:57:20 -0800

    adds search bar sort funtionality

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('snippets/', views.snippets, name="snippets"),
     path('snippets/<int:snippet_id>/unassoc_tag/<int:tag_id>/', views.unassoc_tag, name="unassoc_tag"),
+    path('snippets/<int:snippet_id>/findOrCreate_assoc_tag/', views.findOrCreate_assoc_tag, name="findOrCreate_assoc_tag"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -15,4 +15,15 @@ def unassoc_tag(request, snippet_id, tag_id):
     # unassociate them
     snippet.tags.remove(tag)
     return redirect('snippets')
-    return HttpResponse('<p>unassoc_tag view funciton!</p>')
+
+
+def findOrCreate_assoc_tag(request, snippet_id):
+    # find snippet with provided id
+    snippet = Snippet.objects.get(id=snippet_id)
+    # find or create tag with provided id
+    foundOrCreated_tag = Tag(name=request.POST['tag'])
+    foundOrCreated_tag.save()
+    # tag = Tag.objects.get(id=tag_id)
+    # associate them
+    snippet.tags.add(foundOrCreated_tag)
+    return redirect('snippets')
