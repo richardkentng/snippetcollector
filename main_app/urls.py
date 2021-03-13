fe34563693bffd95391beb67913c78f3b2cde485
Richard Ng <richardkentng@gmail.com>
2021-03-12 23:07:41 -0800

    cadds functionality to unassociate tag from snippet on homepage

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('snippets/', views.snippets, name="snippets"),
+    path('snippets/<int:snippet_id>/unassoc_tag/<int:tag_id>/', views.unassoc_tag, name="unassoc_tag"),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Group, Tag, Snippet
 # Create your views here.
 def snippets(request):
     # query all snippets
     snippets = Snippet.objects.all()
     return render(request, 'snippets/index.html', {'snippets': snippets})
+
+def unassoc_tag(request, snippet_id, tag_id):
+    # find snippet with provided id
+    snippet = Snippet.objects.get(id=snippet_id)
+    # find tag with provided id
+    tag = Tag.objects.get(id=tag_id)
+    # unassociate them
+    snippet.tags.remove(tag)
+    return redirect('snippets')
+    return HttpResponse('<p>unassoc_tag view funciton!</p>')
